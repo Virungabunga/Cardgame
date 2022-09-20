c5c1135896afa226052f2599c814bea8b1dab4b2
oscarsparrman <oscar.sparrman@student.kyh.se>
2022-09-20 11:09:39 +0200

    First commit

---
+++ app/src/main/java/edu/ith/cardgame/MainActivity.kt
@@ -0,0 +1,14 @@
+package edu.ith.cardgame
+
+import android.content.res.Resources
+import android.graphics.drawable.VectorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
